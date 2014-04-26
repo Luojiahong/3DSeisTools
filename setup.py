0c172baa5e23d334c1f6bd0d969a08c5621df662
Malcolm White <mcwhite@ucsd.edu>
2014-04-26 13:10:26 -0700

    Add setup.py for easy installation of pure Python package.

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(name='anfseistools',
+      version='1.0',
+      description='3D Seismic Inversion Utilities',
+      author='Malcolm White',
+      author_email='mcwhite@ucsd.edu',
+      url='https://github.com/malcolmw/3DSeisTools',
+      packages=['anfseistools'])
